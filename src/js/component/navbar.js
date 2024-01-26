549a0e1c5d07dde73c0585ed0259dc2e95c9215d
Runneker74 <122167596+Runneker74@users.noreply.github.com>
2024-01-26 23:50:00 +0000

    favoritos

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -1,17 +1,53 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Link } from "react-router-dom";
+import { Context } from "../store/appContext";
 
 export const Navbar = () => {
-	return (
-		<nav className="navbar navbar-light bg-light mb-3">
-			<Link to="/">
-				<span className="navbar-brand mb-0 h1">React Boilerplate</span>
-			</Link>
-			<div className="ml-auto">
-				<Link to="/demo">
-					<button className="btn btn-primary">Check the Context in action</button>
-				</Link>
-			</div>
-		</nav>
-	);
+  const { store, actions } = useContext(Context);
+
+  return (
+    <nav className="navbar navbar-light bg-light mb-3">
+      <Link to="/">
+        <img
+          width={90}
+          title="Ir a Inicio"
+          src="https://i.pinimg.com/originals/b6/af/5a/b6af5aeff0ee43a4253fc70c167bb6db.png"
+          alt=""
+        />
+      </Link>
+      <div className="ml-auto">
+        <div className="dropdown">
+          <button
+            className="btn btn-primary dropdown-toggle"
+            type="button"
+            id="dropdownMenuButton1"
+            data-bs-toggle="dropdown"
+            aria-expanded="false"
+          >
+            Favorites ({store.favorites.length})
+          </button>
+          <ul
+            className="dropdown-menu dropdown-menu-end"
+            aria-labelledby="dropdownMenuButton1"
+          >
+            {store.favorites && store.favorites.length > 0 ? (
+              store.favorites.map((item) => (
+                <li>
+                  <a className="dropdown-item" href="#">
+                    {item}
+                  </a>
+                </li>
+              ))
+            ) : (
+              <li>
+                <a className="dropdown-item" href="#">
+                  No Favorites
+                </a>
+              </li>
+            )}
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
 };
